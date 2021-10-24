9ce142702fdd158682a1be5c9943d94d9f61b109
Leandro Fernandes <leanstorm@hotmail.com>
2021-10-24 12:39:19 -0300

    WB-001 - Modifica classe de teste

--- src/test/java/tests/InformacoesUsuarioPageObjectsTest.java
+++ src/test/java/tests/InformacoesUsuarioPageObjectsTest.java
@@ -20,7 +20,7 @@ public class InformacoesUsuarioPageObjectsTest {
 
     @Before
     public void setUp(){
-        navegador = Web.createBrowserStack();
+        navegador = Web.createChrome();
     }
 
     @Test
